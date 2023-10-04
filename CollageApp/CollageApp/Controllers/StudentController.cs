fcdb461d1eb9fc4730e7bd3fa9747d037dd62b4d
Quy King <hoangquy3125@gmail.com>
2023-10-04 18:13:57 +0700

    project

--- CollageApp/CollageApp/Controllers/StudentController.cs
+++ CollageApp/CollageApp/Controllers/StudentController.cs
@@ -37,13 +37,45 @@ namespace CollageApp.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
 
-        public ActionResult<StudentDTO> GetStudentById (int id)
+        public ActionResult<StudentDTO> GetStudentById(int id)
         {
             if (id <= 0)
                 return BadRequest($"Vui long nhap id > 0");
             var x = CollageRepository.Students.Where(x => x.Id == id).FirstOrDefault();
             if (x == null)
                 return NotFound($"Khong tim thay id la {x}");
+            return Ok(x);
+        }
+
+        [HttpGet]
+        [Route("{name:alpha}", Name = "GetName")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<StudentDTO> GetStudentByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return BadRequest("Loi!");
+            var n = CollageRepository.Students.Where(n => n.Name == name).FirstOrDefault();
+            if (n == null)
+                return NotFound("Khong tim thay ten");
+            return Ok(n);
+        }
+
+        [HttpDelete]
+        [Route("delete/{id:int}", Name = "DeleteId")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<StudentDTO> DeleteById(int id)
+        {
+            if (id < 0)
+                return BadRequest("Id can lon hon 0");
+            var x = CollageRepository.Students.Where(n =>n.Id == id).FirstOrDefault();
+            if (x == null) return NotFound();
+            CollageRepository.Students.Remove(x);
             return Ok(x);
         }
     }
